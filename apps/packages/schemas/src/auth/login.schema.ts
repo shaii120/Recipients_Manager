dd2c23207572c6d5bb8cbab80e7923709e128c23
shaii120 <kofmisar@gmail.com>
2026-03-24 22:22:15 +0200

    Adding clear error messages

--- apps/packages/schemas/src/auth/login.schema.ts
+++ apps/packages/schemas/src/auth/login.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const LoginSchema = z.object({
     email: z.email(),
-    password: z.string()
+    password: z.string().nonempty("Password is required")
 });
 
 export type LoginInput = z.infer<typeof LoginSchema>;
--- apps/packages/schemas/src/auth/register.schema.ts
+++ apps/packages/schemas/src/auth/register.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const RegisterSchema = z.object({
     email: z.email(),
-    password: z.string().min(6)
+    password: z.string().min(6, "Password must be at least 6 characters")
 });
 
 export type RegisterInput = z.infer<typeof RegisterSchema>;
